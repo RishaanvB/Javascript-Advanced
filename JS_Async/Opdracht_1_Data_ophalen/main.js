db2367c79e0433872f1b02048c1b63f79fd9a0b6
Rishaan van Beveren <rishaanvanbeveren@gmail.com>
2020-10-05 22:07:28 +0200

    commit opdracht 1+2 die eigenlijk niet hoeven ingeleverd te worden :)

---
+++ JS_Async/Opdracht_1_Data_ophalen/main.js
@@ -0,0 +1,9 @@
+fetch("http://swapi.dev/api/planets/1/")
+  .then((response) => response.json())
+  .then((myJson) => {
+    document.body.innerHTML =
+      "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>";
+  });
+
+
+  console.log();
