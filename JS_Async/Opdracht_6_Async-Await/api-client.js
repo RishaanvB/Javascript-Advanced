0f19df605edfa27c798a856aa7860de90c650fdc
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2020-11-02 17:50:20 +0100

    commit async/await backup usb

--- JS_Async/Opdracht_6_Async-Await/api-client.js
+++ JS_Async/Opdracht_6_Async-Await/api-client.js
@@ -1,10 +1,57 @@
-const getData = async () => {
+const API_KEY = "dc4d5dc6ccdc44a3d2cde585c687d1db";
+const getMoviesGenre = async () => {
   try {
-	const API_KEY = "dc4d5dc6ccdc44a3d2cde585c687d1db"; 
-	const apiUrl =
-      `https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}`;
+    const apiUrl = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
     let reponse = await fetch(apiUrl, { method: "GET" });
     let data = await reponse.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getFaveMovie = async () => {
+  try {
+    const imdbID = "tt0398286"; //tangled
+    const apiUrl = `https://api.themoviedb.org/3/find/${imdbID}?api_key=${API_KEY}&language=en-US&external_source=imdb_id`;
+    let response = await fetch(apiUrl, { method: "GET" });
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getTopRatedMovies = async () => {
+  try {
+    const apiUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&&page=1`;
+    let response = await fetch(apiUrl, { method: "GET" });
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getTopRatedbyGenre = async () => {
+  try {
+    const genreID = 28;
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&with_genres=${genreID}`;
+    let response = await fetch(apiUrl, { method: "GET" });
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getTopRatedbyYear = async () => {
+  try {
+    const yearRelease = ;
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=vote_average.desc&include_adult=false&include_video=false&page=1&primary_release_year
+=1975    `;
+    let response = await fetch(apiUrl, { method: "GET" });
+    let data = await response.json();
     return data;
   } catch (error) {
     console.log(error);
--- JS_Async/Opdracht_6_Async-Await/script.js
+++ JS_Async/Opdracht_6_Async-Await/script.js
@@ -1,10 +1,74 @@
-const processData = async () => {
+// const processData = async () => {
+//   try {
+//     let fetchedData = await getData();
+//     console.log(fetchedData);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// console.log(processData());
+//opdarcht 1
+const setMoviesGenre = async () => {
   try {
-    let fetchedData = await getData();
-    console.log(fetchedData);
+    let genreData = await getMoviesGenre();
+    // console.log(genreData);
+    genreData.genres.map((element) => {
+      const genreList = document.querySelector("#genrelist");
+      const genreListItem = document.createElement("li");
+      genreList.appendChild(genreListItem);
+      genreListItem.innerHTML = `${element.name},  ID: ${element.id}`;
+    });
   } catch (error) {
     console.log(error);
   }
 };
 
-console.log(processData());
+setMoviesGenre();
+
+//opdarcht 2
+
+const setFaveMovie = async () => {
+  let faveMovieData = await getFaveMovie();
+  // console.log(faveMovieData.movie_results);
+  // console.log(faveMovieData.movie_results[0].title);
+  const newH1 = document.getElementById("favemovie");
+  newH1.innerHTML = faveMovieData.movie_results[0].title;
+};
+
+setFaveMovie();
+
+//opdracht 3 top rated movies
+
+const setTopRatedMovies = async () => {
+  try {
+    const topRated = document.getElementById("toprated");
+    const data = await getTopRatedMovies();
+    const topTen = data.results.splice(0, 10);
+    topTen.forEach((movie) => {
+      const newLi = document.createElement("li");
+      topRated.appendChild(newLi);
+      newLi.innerHTML = `Title: ${movie.title} Score: ${movie.vote_average} `;
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+setTopRatedMovies();
+
+//opdracht 4
+const setTopRatedByGenre = async () => {
+  const data = await getTopRatedbyGenre();
+  console.log(data);
+  const topRatedyGenreList = document.getElementById("topratedbygenre");
+  genreList = data.results.splice(0, 10);
+
+  genreList.forEach((movie) => {
+    const newLi = document.createElement("li");
+    topRatedyGenreList.appendChild(newLi);
+    newLi.innerHTML = `Title: ${movie.title}, Score: ${movie.vote_average}`
+  });
+};
+
+setTopRatedByGenre();
