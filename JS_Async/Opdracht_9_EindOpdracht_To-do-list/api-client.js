6c1bd004ec33e2ba1e7266b4584304967a19de0f
Rishaan van Beveren <rishaan.vanbeveren@gmail.com>
2020-11-12 22:50:04 +0100

    helemaal opnieuw begonnennnnn...

--- JS_Async/Opdracht_9_EindOpdracht_To-do-list/api-client.js
+++ JS_Async/Opdracht_9_EindOpdracht_To-do-list/api-client.js
@@ -1,12 +1,14 @@
 const boxID = "box_f1b4aec33905771a9ab2"
-const recordID = "5faae6b27b86a40017f72f87"
-
 
 const apiUrl = `https://jsonbox.io/${boxID}/`
+
+
 const getData = async () => {
     try {
+        console.log("retreiving data");
         let response = await fetch(apiUrl, { method: "GET" });
         let data = await response.json();
+        console.log("returning data");
         return data
     } catch (error) {
         console.log(error)
@@ -14,18 +16,36 @@ const getData = async () => {
 
 }
 
-const postData = (newTask) => {
+const postData = async (newTask) => {
     try {
         const data = { description: newTask, done: false };
-        fetch(apiUrl, {
+        console.log(data, "data logged to api")
+        let response = await fetch(apiUrl, {
             method: "POST",
             body: JSON.stringify(data),
             headers: {
                 "Content-Type": "application/json",
             },
         })
+        let result = await response.json()
+        console.log(result);
+
     } catch (error) {
         console.log(error);
     }
 };
 
+const removeData = async (postID) => {
+    try {
+        console.log(`trying to remove post with ID ===>${postID} `);
+        console.log(`trying to access? ${apiUrl}${postID}`)
+
+        let response = await fetch(`${apiUrl}${postID}`, {
+            method: "DELETE",
+        })
+        let result = await response.json()
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
--- JS_Async/Opdracht_9_EindOpdracht_To-do-list/script.js
+++ JS_Async/Opdracht_9_EindOpdracht_To-do-list/script.js
@@ -3,95 +3,71 @@ const removeAllBtn = document.getElementById("remove-all-btn")
 const addBtn = document.getElementById("add-task-btn")
 const taskInput = document.getElementById("input-field")
 
-// gets data from apiUrl 
+// gets data from apiUrl and sets to dom
 const setData = async () => {
-    try {
-        const taskData = await getData();
-        console.log(taskData);
-        displayTasksToDom(taskData)
 
-    } catch (error) {
-        console.log(error);
-    }
-}
+    const taskData = await getData();
+    console.log("logs array of tasks", taskData);
 
-// retrieves data from api??? on pageload
-window.onload = setData()
+    // Kan het zonder onderstaande regel???????   
+    taskUl.querySelectorAll("*").forEach((task) => task.remove());
 
 
+    taskData.forEach(task => {
 
-removeAllBtn.addEventListener("click", () => {
-    taskUl.querySelectorAll("*").forEach((listItem) => listItem.remove())
-})
-
-
-const createTasks = () => {
-    let newDiv = document.createElement("div")
-    let newLi = document.createElement("li");
-    let deleteLi = document.createElement("img")
-    deleteLi.setAttribute("src", "noun_Trash_3595480.png")
-    taskUl.prepend(newDiv)
-    newDiv.append(newLi, deleteLi)
-    let inputValue = taskInput.value
-    newLi.innerHTML = inputValue
-    postData(inputValue)
-
-
-    deleteLi.addEventListener("click", (e) => {
-        // console.log(task._id);
-        e.target.parentElement.remove();
-
-
-
-    })
-
-
-}
-
-
-// displays task description from api into dom on pageload
-const displayTasksToDom = array => {
-
-
-    array.forEach(task => {
         let newDiv = document.createElement("div")
         let newLi = document.createElement("li");
         let deleteLi = document.createElement("img")
         deleteLi.setAttribute("src", "noun_Trash_3595480.png")
-        taskUl.appendChild(newDiv)
+        deleteLi.setAttribute("class", "trash-icon")
+        deleteLi.setAttribute("id", task._id)
+        taskUl.prepend(newDiv)
         newDiv.append(newLi, deleteLi)
         newLi.innerHTML = task.description;
-
-        // remove button functionality
-        deleteLi.addEventListener("click", (e) => {
-            console.log(task._id);
-            console.log(e.target.parentElement);
-            e.target.parentElement.remove();
-
-
-
-
-        })
-
     })
 }
-// button that adds new task
-addBtn.addEventListener("click", () => {
-    createTasks()
 
+
+window.onload = setData()
+
+
+// button that adds new task
+addBtn.addEventListener("click", async () => {
+    let inputValue = taskInput.value
+    await postData(inputValue)
+    await setData()
 })
 
 // add task on enter
-taskInput.addEventListener("keydown", (event) => {
+taskInput.addEventListener("keydown", async event => {
     if (event.code === "Enter") {
-        createTasks()
+        let inputValue = taskInput.value
+        await postData(inputValue)
+        await setData()
+    }
+})
+
+
+// Remove item in the list
+
+taskUl.addEventListener("click", async (e) => {
+    // await setData()
+    if (e.target.className === "trash-icon") {
+        let postID = e.target.id
+        await removeData(postID)
+        await setData()
     }
 })
 
 
 
-
-
+removeAllBtn.addEventListener("click", () => {
+    taskUl.querySelectorAll(".trash-icon").forEach(async task => {
+        await removeData(task.id)
+        await setData()
+        
+    })
+})
 
 
 
